22370c3a70b6dfd41aa492434868dd58c7ee39a0
Tony Coculuzzi <tonycoculuzzi@gmail.com>
2021-05-04 15:42:16 -0400

    Update csharp-basic.cs

--- csharp-basic.cs
+++ csharp-basic.cs
@@ -1,9 +1,10 @@
 // At the top of each file should be any `usings` or `includes`, sorted by name
 using System;
 
-// Each class should exist in a namespace
+// Each class should exist in a namespace. Failing to add a namespace may cause compilation conflicts.
 // For most of our projects, game-specific code exists in the Game namespace, or in a child of Game (Game.Player, Game.Enemies, etc)
 namespace Game
+// Opening brackets should exist alone on a new line
 {
 	// Nothing should come between your namespace declaration and your class
 	public class ExampleClass : MonoBehaviour
@@ -21,7 +22,6 @@ namespace Game
 		// For example: OnEnable, Awake, Start methods would be at the top, with Update below them, followed by OnDisable, OnDestroy, etc
 		void Awake()
 		{
-			// code
 		}
 
 		void Start()
@@ -37,7 +37,6 @@ namespace Game
 
 		void OnDisable()
 		{
-			// code
 		}
 
 		// Under your MonoBehaviour methods is where the rest of your methods/functions should go.
@@ -45,17 +44,14 @@ namespace Game
 		// These should be listed in order of execution
 		private void StartFunction1()
 		{
-			// code
 		}
 
 		private void UpdateExampleFunction1(bool exampleArgument1)
 		{
-			// code
 		}
 
 		private void UpdateExampleFunction2(bool exampleArgument1, int exampleArgument2)
 		{
-			// code
 		}
 	}
 }
